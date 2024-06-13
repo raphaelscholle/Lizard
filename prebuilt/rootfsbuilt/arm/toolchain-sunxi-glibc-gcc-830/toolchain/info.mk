07ab81700b312001486d07216150c3bd49af2e6d
raphaelscholle <raphael@openhdfpv.org>
2024-06-13 21:09:32 +0200

    add stuff

---
+++ prebuilt/rootfsbuilt/arm/toolchain-sunxi-glibc-gcc-830/toolchain/info.mk
@@ -0,0 +1,6 @@
+TARGET_CROSS=arm-openwrt-linux-gnueabi-
+GCC_VERSION=8.3.0-2021.08
+LIBC_TYPE=glibc
+LIBC_URL=http://www.gnu.org/software/libc/
+LIBC_VERSION=2.29
+LIBC_SO_VERSION=2.29
---
+++ prebuilt/rootfsbuilt/arm/toolchain-sunxi-musl-gcc-830/toolchain/lib/gcc/arm-openwrt-linux-muslgnueabi/8.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/xuqiang/workspace/tina-old/out/v853-perf1/compile_dir/toolchain/gcc-8.3.0/configure --with-bugurl=https://dev.openwrt.org/ --with-pkgversion='OpenWrt/CodeSourcery GCC 8.3.0 2021.08' --prefix=/home/xuqiang/workspace/tina-old/out/v853-perf1/staging_dir/toolchain --build=x86_64-linux-gnu --host=x86_64-linux-gnu --target=arm-openwrt-linux-muslgnueabi --with-gnu-ld --enable-target-optspace --enable-libgomp --disable-libmudflap --disable-multilib --disable-nls --without-isl --without-cloog --with-host-libstdcxx=-lstdc++ --with-gmp=/home/xuqiang/workspace/tina-old/out/host --with-mpfr=/home/xuqiang/workspace/tina-old/out/host --with-mpc=/home/xuqiang/workspace/tina-old/out/host --disable-decimal-float --with-diagnostics-color=auto-if-env --disable-libssp --enable-__cxa_atexit --with-cpu=cortex-a7 --with-fpu=neon --with-float=hard --with-headers=/home/xuqiang/workspace/tina-old/out/v853-perf1/staging_dir/toolchain/include --disable-libsanitizer --enable-languages=c,c++,fortran --enable-shared --enable-threads --with-slibdir=/home/xuqiang/workspace/tina-old/out/v853-perf1/staging_dir/toolchain/lib --enable-lto --with-libelf=/home/xuqiang/workspace/tina-old/out/host";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "cortex-a7" }, { "float", "hard" }, { "fpu", "neon" }, { "tls", "gnu" } };
